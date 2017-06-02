6ad01e592d2b4c9059ec517a4b0c20063c2d9789
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-06-01 17:47:49 -0700

    Added render template function to index handler

--- handlers/index.go
+++ handlers/index.go
@@ -1,12 +1,22 @@
 package handlers
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 	"os"
-
-	"github.com/isomorphicgo/isogoapp/view"
 )
 
+// Template rendering function
+func RenderTemplate(w http.ResponseWriter, templateFile string, templateData interface{}) {
+
+	t, err := template.ParseFiles(templateFile)
+	if err != nil {
+		log.Fatal("Error encountered while parsing the template: ", err)
+	}
+	t.Execute(w, templateData)
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	view.RenderTemplate(w, os.Getenv("ISOGO_APP_ROOT")+"/templates/index.tmpl", nil)
+	RenderTemplate(w, os.Getenv("ISOGO_APP_ROOT")+"/templates/index.tmpl", nil)
 }
